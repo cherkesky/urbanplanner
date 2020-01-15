d98046ed9674a3da1c9595f804c244d7ba6a01d7
Cherkesky <guy@cherkesky.com>
2020-01-15 15:50:23 -0600

    built an little empire there

--- building.py
+++ building.py
@@ -45,9 +45,32 @@ class Building:
   def print_building(self):
     print(f'{self.address} was purchased by {self.owner} on {self.date_constructed} and has {self.stories} stories')
 
-dwell = Building("1234 2nd Ave S", "2")
-dwell.construct()
-dwell.purchase("Bill Gates")
-dwell.print_building()
+dwell1 = Building("1234 2nd Ave S #4", "2")
+dwell1.construct()
+dwell1.purchase("Bill Gates")
+dwell1.print_building()
 
+dwell2 = Building("1234 2nd Ave S #7", "2")
+dwell2.construct()
+dwell2.purchase("Bill Gates")
+dwell2.print_building()
 
+dwell3 = Building("1234 2nd Ave S #13", "2")
+dwell3.construct()
+dwell3.purchase("Bill Gates")
+dwell3.print_building()
+
+dwell2 = Building("1234 2nd Ave S #7", "2")
+dwell2.construct()
+dwell2.purchase("Bill Gates")
+dwell2.print_building()
+
+clifton1 = Building("620 27th Ave N", "3")
+clifton1.construct()
+clifton1.purchase("Bill Gates")
+clifton1.print_building()
+
+clifton2 = Building("622 27th Ave N", "3")
+clifton2.construct()
+clifton2.purchase("Bill Gates")
+clifton2.print_building()
